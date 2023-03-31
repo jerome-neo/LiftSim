fc24f0e8eb8b80c13438f1859cd7b75d4e6bd7e0
pintubread <dorothyboyle@u.nus.edu>
2023-03-31 23:22:32 +0800

    modified files from current Otis simulation so to enable merging later

--- src/main/python/simulation/Building.py
+++ src/main/python/simulation/Building.py
@@ -2,112 +2,141 @@ import simpy
 import random
 from Floor import TopFloor, GroundFloor, SandwichFloor
 import Person
-import ElevatorSystem
+from ElevatorSystem import ElevatorSystem
 import LiftRandoms
-import ModernEGCS
+from ModernEGCS import ModernEGCS
 import numpy as np
-
 
 class Building(object):
     """
     A class representing a building.
-
     Attributes:
         env (simpy.Environment): The simulation environment.
         num_up (int): The number of elevators going up.
         num_down (int): The number of elevators going down.
-        elevators (simpy.Resource): The elevators in the building.
         num_floors (int): The number of floors in the building.
         floors (list): A list containing all floors in the building.
         elevator_group (ElevatorSystem.ElevatorSystem): An instance of the ElevatorSystem class that manages the
             elevators in the building.
-        all_persons_spawned (list): A list containing all Person instances created and placed in the building.
-
+        all_persons_spawned (PersonList): A PersonList object containing all Person instances.
     Methods:
         get_num_floors(): Returns the number of floors in the building.
+        get_all_persons(): Returns the PersonList object in the building.
+        place_person_on_floor(): Distribute Person from PersonList into the floors.
         initialise(): Initialises the building by adding the floors and the elevator system.
         simulate(): Simulates the building operation by creating Person instances, placing them in their
             respective floors, and managing the elevators in the building.
-
     """
-    def __init__(self, env, num_up=0, num_down=0, num_elevators=0, num_floors=0):
+    def __init__(self, env, num_up, num_down, num_floors, persons_list):
         """
         Args:
             env (simpy.Environment): The simulation environment.
             num_up (int): The number of elevators going up.
             num_down (int): The number of elevators going down.
-            num_elevators (int): The total number of elevators in the system
             num_floors (int): The number of floors in the building.
-
+            persons_list (PersonList): The generated Persons used for simulation.
         """
         self.env = env
         self.num_up = num_up
         self.num_down = num_down
-        self.num_elevators = num_elevators
         self.num_floors = num_floors
         self.floors = []
         self.elevator_group = None
-        self.all_persons_spawned = []
+        self.all_persons_spawned = persons_list
         self.arrival_rates_floors = np.zeros(num_floors)
-
+        self.elevator_algo = None
+    
+    def get_elevator_system(self):
+        """Returns either ElevatorSystem or ModernEGCS object which is implemented as the building's elevator system"""
+        return self.elevator_group
+    
     def get_num_floors(self) -> int:
         """Returns the number of floors in the building."""
         return self.num_floors
 
-    def initialise(self,lift_algo) -> None:
-        """Initialises the building by adding the floors and the elevator system."""
-        self.floors.append(GroundFloor(self.env,self,1))
-        self.floors.extend([SandwichFloor(self.env,self,i) for i in range(2, self.num_floors)])
-        self.floors.append(TopFloor(self.env,self,self.num_floors))
+    def get_all_persons(self) -> list:
+        """Returns a list of Person objects that have been instantiated."""
+        return self.all_persons_spawned
 
-        if lift_algo=="Otis":
-            self.elevator_group = ElevatorSystem.ElevatorSystem(env=self.env, floors=self.floors, num_up=self.num_up, num_down=self.num_down)
-        elif lift_algo=="ModernEGCS":
-            self.elevator_group = ModernEGCS.ModernEGCS(env=self.env, floors=self.floors, num_elevators=self.num_elevators,w1=1,w2=1,w3=1)
+    def place_person_on_floor(self, person) -> None:
+        """
+        Places person into the simulation floor.
+        Args:
+            person (Person): The person who wants to use the elevator system.
+        """
+        # Put the person in the floor and call the lift
+        call_direction = person.get_direction()
+        curr_floor = self.floors[person.get_curr_floor() - 1]
+        if call_direction == "DOWN":
+            curr_floor.add_person_going_down(person)
+        else:
+            curr_floor.add_person_going_up(person)
+    
+    def initialise(self,elevator_algo) -> None:
+        """Initialises all components that make up the building"""
+        # Place floors into building
+        self.floors.append(GroundFloor(self.env, self, 1))
+        self.floors.extend([SandwichFloor(self.env, self, i) for i in range(2, self.num_floors)])
+        self.floors.append(TopFloor(self.env, self, self.num_floors))
 
+        # Place elevators into building
+        #self.elevator_group = ElevatorSystem(self.env, self.floors, self.num_up, self.num_down)
+
+        if elevator_algo=="Otis":
+            self.elevator_algo = "Otis"
+            self.elevator_group = ElevatorSystem(self.env, self.floors, self.num_up, self.num_down)
+        elif elevator_algo=="ModernEGCS":
+            self.elevator_algo = "ModernEGCS"
+            self.elevator_group = ModernEGCS(env=self.env, floors=self.floors, num_elevators=self.num_elevators,w1=1,w2=1,w3=1)
+
+        # Place persons into building
+        for person in self.all_persons_spawned.get_person_list():
+            self.place_person_on_floor(person)
 
     def simulate(self) -> None:
         """
-        Simulates the building operation by creating Person instances, placing them in their respective floors, and managing the elevators in the building.
-
-        Args:
-            -
-
+        Simulates the building operation by creating Person instances, placing them in their respective floors,
+        and managing the elevators in the building.
         Yields:
                 The arrival time of each wave of Person instances.
-
         """
-        random_variable_generator = LiftRandoms.LiftRandoms()
-        index = 0
-        while True:
-            # Generate arrive time of a Wave
-            inter_arrival_time = random_variable_generator.next_arrival_time(self.env.now)
-            yield self.env.timeout(inter_arrival_time)
-    
-            index += 1  # update numbering
-            # Generate person
-            person = Person.Person(self.env, index, self)
-            
-            self.all_persons_spawned.append(person)  # for calculating waiting time
-            self.elevator_group.update_status()
 
-            # Otis handling of persons
-            if isinstance(self.elevator_group, ElevatorSystem.ElevatorSystem):
-                self.elevator_group.handle_person(person) #handle each incoming person
-                self.env.process(self.elevator_group.handle_rising_call())
-                self.env.process(self.elevator_group.handle_landing_call())
+        while True:
+            print(f'Current simulation time: {self.env.now}')
+            if self.env.now == 0 or self.elevator_group.is_all_idle():
+                next_arrival_time = self.all_persons_spawned.get_earliest_arrival_time()
+                if next_arrival_time >= self.env.now:
+                    print(f"Fast-forwarding simulation time by {round(next_arrival_time - self.env.now)} unit(s)...")
+                    yield self.env.timeout(round(next_arrival_time - self.env.now))
+            # activate floor buttons if person 'arrived'
+            for floor in self.floors:
+                floor.update()
+
+            if self.elevator_algo == "Otis":
+                self.elevator_group.allocate_rising_call()
+                self.elevator_group.allocate_landing_call()
 
                 for elevator in self.elevator_group.elevators_up:
-                    self.env.process(elevator.activate())
-                
+                    # print(f"{elevator} with path status: {elevator.has_path()}")
+                    yield self.env.process(elevator.activate())
+                    yield self.env.process(elevator.move())
+
                 for elevator in self.elevator_group.elevators_down:
-                    self.env.process(elevator.activate())
+                    # print(f"{elevator} path status: {elevator.has_path()}")
+                    yield self.env.process(elevator.activate())
+                    yield self.env.process(elevator.move())
+
+                self.elevator_group.update_status()
+                yield self.env.timeout(1)
+                print(self.elevator_group.print_system_status())
+                print(f'\n')
 
             #ModernEGCS handling of persons
-            elif isinstance(self.elevator_group, ModernEGCS.ModernEGCS):
-                self.elevator_group.handle_person(person)
+            elif self.elevator_algo == "ModernEGCS":
+                self.elevator_group.assign_calls()
                 for elevator in self.elevator_group.elevators:
-                    self.env.process(elevator.activate)
+                    self.env.process(elevator.activate())
+                    self.env.process(elevator.move())
 
             else:
                 print("Lift algorithm has not been configured yet")
--- src/main/python/simulation/Floor/SandwichFloor.py
+++ src/main/python/simulation/Floor/SandwichFloor.py
@@ -1,28 +1,27 @@
-from .Floor import Floor
 
+from .Floor import Floor
+import HallCall
+import ModernEGCS
 
 class SandwichFloor(Floor):
     """A class representing a floor of a building with people going up and down,
     which is a subclass of the Floor class."""
-    def __init__(self, index: int):
+    def __init__(self, env, building, index: int):
         """
         Initialize the sandwich floor with the given index.
-
         Args:
+            env (simpy.Environment): The simulation environment.
             index (int): The index of the sandwich floor.
-
         """
-        super().__init__(index)
+        super().__init__(env, building, index)
         self.going_up_persons = []
         self.going_down_persons = []
 
     def __str__(self):
         """
         Return a string representation of the sandwich floor.
-
         Returns:
             str: A string representation of the sandwich floor.
-
         """
         return f"Floor {self.floor_index} has " \
                f"{len(self.going_up_persons)} person(s) going up and " \
@@ -37,44 +36,56 @@ class SandwichFloor(Floor):
 
         """
         self.going_up_persons.append(person)
-
+    
     def add_person_going_down(self, person) -> None:
         """
         Add a person who wants to go down from the sandwich floor.
-
             Args:
                 person (Person): The person who wants to go down from the sandwich floor.
-
         """
         self.going_down_persons.append(person)
 
+
     def remove_all_persons_going_up(self) -> list:
         """
-        Remove all persons who want to go up from the sandwich floor and return them as a list.
-
+        Remove all persons who want to go up from the ground floor and return them as a list.
         Returns:
-            List[Person]: A list of all persons who want to go up from the sandwich floor.
-
+            List[Person]: A list of all persons who want to go up from the ground floor.
         """
         pointer = []
-        n = len(self.going_up_persons)
-        for i in range(n):
-            pointer.append(self.going_up_persons.pop())
-        return pointer
+        if not self.has_call_up() or self.going_up_persons[0].get_arrival_time() > self.env.now:
+            return pointer
+        else:
+            n = len(self.going_up_persons)
+            count = 0
+            for i in range(n):
+                if self.going_up_persons[i].get_arrival_time() > self.env.now:
+                    break
+                count += 1
+            pointer = self.going_up_persons[:count]
+            self.going_up_persons = self.going_up_persons[count + 1:]
+            return pointer
 
     def remove_all_persons_going_down(self) -> list:
         """
         Remove all persons who want to go down from the sandwich floor and return them as a list.
-
         Returns:
             List[Person]: A list of all persons who want to go down from the sandwich floor.
-
         """
         pointer = []
-        n = len(self.going_down_persons)
-        for i in range(n):
-            pointer.append(self.going_down_persons.pop())
-        return pointer
+        if not self.has_call_up() or self.going_down_persons[0].get_arrival_time() > self.env.now:
+            return pointer
+        else:
+            n = len(self.going_down_persons)
+            count = 0
+            for i in range(n):
+                if self.going_down_persons[i].get_arrival_time() > self.env.now:
+                    break
+                count += 1
+            pointer = self.going_down_persons[:count]
+            self.going_down_persons = self.going_down_persons[count + 1:]
+            return pointer
+
     
     def get_all_persons_going_up(self) -> list:
         """
@@ -83,6 +94,30 @@ class SandwichFloor(Floor):
             List[Person]: A list of all persons who want to go up from the sandwich floor.
         """
         return self.going_up_persons
+    
+    def sort(self) -> None:
+        """Sorts the list of Persons in ascending arrival time."""
+        self.going_up_persons.sort(key=lambda person: person.get_arrival_time())
+        self.going_down_persons.sort(key=lambda person: person.get_arrival_time())
+
+    def update(self) -> None:
+        """Important to call this method every step of the simulation to update call status of every floor."""
+        # Floor will "check" if people have arrived by peeking at the simulation time
+        # to compare with the person's arrival time.
+        building = self.get_building()
+        system = building.get_elevator_system()
+        if len(self.going_up_persons) != 0 and self.going_up_persons[0].get_arrival_time() <= self.env.now:
+            self.set_call_up()
+            self.person_arrived()
+            if isinstance(system,type(ModernEGCS)):
+                hall_call = HallCall(self.env,self.floor_index,1)
+                system.add_hall_call(hall_call)
+        if len(self.going_down_persons) != 0 and self.going_down_persons[0].get_arrival_time() <= self.env.now:
+            self.set_call_down()
+            self.person_arrived()
+            if isinstance(system,type(ModernEGCS)):
+                hall_call = HallCall(self.env,self.floor_index,-1)
+                system.add_hall_call(hall_call)
     
     
     def get_all_persons_going_down(self) -> list:
