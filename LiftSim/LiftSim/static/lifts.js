917253bec4f7299e6443ef96735a2d3f008be62b
Wang-Zixuann <e0492448@u.nus.edu>
2023-03-31 16:21:43 +0800

    update the elevator and input box

--- LiftSim/LiftSim/static/lifts.js
+++ LiftSim/LiftSim/static/lifts.js
@@ -52,3 +52,45 @@ function getDistance(liftNumber, floorNumber) {
   // Calculate distance between lift and floor
   const liftPosition = (liftNumber - 1) * 3 + 2;
 }
+
+const elevatorStatus = [
+  [0, 0, 0, 0, 0, 0, 0, 0, 0], // Elevator 1
+  [0, 0, 0, 0, 0, 0, 0, 0, 0], // Elevator 2
+  [0, 0, 0, 0, 0, 0, 0, 0, 0]  // Elevator 3
+];
+
+function setTargetFloor(elevatorNumber, floorNumber) {
+  elevatorStatus[elevatorNumber - 1][floorNumber - 1] = 1;
+  // 获取对应楼层的 DOM 元素
+  const floorElement = document.getElementById(`e${elevatorNumber}f${floorNumber}`);
+  // 修改 DOM 元素的样式
+  floorElement.classList.add('target');
+}
+
+function rmTargetFloor(elevatorNumber, floorNumber) {
+  elevatorStatus[elevatorNumber - 1][floorNumber - 1] = 1;
+  // 获取对应楼层的 DOM 元素
+  const floorElement = document.getElementById(`e${elevatorNumber}f${floorNumber}`);
+  // 修改 DOM 元素的样式
+  floorElement.classList.remove('target');
+}
+
+function turnOnLight(elevatorNumber, floorNumber) {
+  elevatorStatus[elevatorNumber - 1][floorNumber - 1] = 1;
+  // 获取对应楼层的 DOM 元素
+  const floorElement = document.getElementById(`e${elevatorNumber}f${floorNumber}`);
+  // 修改 DOM 元素的样式
+  floorElement.classList.add('on');
+}
+
+// 熄灭某个电梯某个楼层的灯
+function turnOffLight(elevatorNumber, floorNumber) {
+  elevatorStatus[elevatorNumber - 1][floorNumber - 1] = 0;
+  // 获取对应楼层的 DOM 元素
+  const floorElement = document.getElementById(`e${elevatorNumber}f${floorNumber}`);
+  // 修改 DOM 元素的样式
+  floorElement.classList.remove('on');
+}
+
+setTargetFloor(1, 4)
+turnOnLight(2, 5)
