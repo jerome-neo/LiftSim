ec457e60dc0f91a5f90fd9cc782e42cc61feb124
Wang-Zixuann <e0492448@u.nus.edu>
2023-04-03 20:56:16 +0800

    Connect with the backend API

--- LiftSim/LiftSim/index/forms.py
+++ LiftSim/LiftSim/index/forms.py
@@ -6,7 +6,6 @@ from wtforms.validators import DataRequired, EqualTo, NumberRange
 
 
 class addRequestForm(FlaskForm):
-    num_of_pax = IntegerField('num_of_pax', validators=[DataRequired(), NumberRange(0)])
     arrival_time = IntegerField('arrival_time')
     src = IntegerField('src', validators=[DataRequired(), NumberRange(1, 10)])
     dest = IntegerField('dest', validators=[DataRequired(), NumberRange(1, 10)])
