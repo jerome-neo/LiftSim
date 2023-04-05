0fd2878236b1e2cf373445ba078f24ecfcc21d0c
kevinchs0808 <kevinchs0808@gmail.com>
2023-04-05 19:40:34 +0800

    Fixed the API to suit the frontend needs

--- src/main/python/simulation/Main.py
+++ src/main/python/simulation/Main.py
@@ -36,7 +36,7 @@ class Main(object):
         self.person_list = None
         self.lift_algos = ['Otis', 'ModernEGCS']
 
-    def run(self, duration, mode='default'):
+    def run(self, duration, mode):
         """
         Runs the simulation for a specified duration.
 
@@ -116,8 +116,9 @@ class Main(object):
                     'end_time': end_time,
                     'wait_time': wait_time
                 })
-        with open(path + name, 'w', encoding='UTF8') as f:
-            json.dump(data, f, indent=4)
+        json_serializable = json.dumps(data, indent=4)
+        with open(path + name, 'w') as f:
+            f.write(json_serializable)
 
     def output_elevator_log_to_json(self, lift_algo, path='../../out/'):
         name = 'output_elevator_'+lift_algo+'.json'
@@ -130,8 +131,8 @@ class Main(object):
 
 # Step 1
 # set up the environment with number of UP elevators, number of DOWN elevators and number of floors"
-Test = Main(num_up=2, num_down=1, num_floors=9)
+# Test = Main(num_up=2, num_down=1, num_floors=9)
 
 # Step 2
 # run the simulation by telling it how long to run, e.g. 6800 (from 6 am to 12 am at the same day)
-Test.run(64800, mode='manual')
+# Test.run(64800, mode='manual')
